7cce882b7b7cb76c134e73016ece401eb579254d
marcel-hinderlich-1314-fhb <marcelh89@googlemail.com>
2013-10-28 18:24:10 +0100

    package change

--- grails-app/domain/de/sixfourpixel/web106/page/Page.groovy
+++ grails-app/domain/de/sixfourpixel/web106/page/Page.groovy
@@ -1,4 +1,4 @@
-package de.sixfourpixel.web106
+package de.sixfourpixel.web106.page
 
 class Page {
 
--- test/unit/de/sixfourpixel/web106/PageControllerSpec.groovy
+++ test/unit/de/sixfourpixel/web106/PageControllerSpec.groovy
@@ -2,6 +2,8 @@ package de.sixfourpixel.web106
 
 
 
+import de.sixfourpixel.web106.page.Page;
+import de.sixfourpixel.web106.page.PageController;
 import grails.test.mixin.*
 import spock.lang.*
 
