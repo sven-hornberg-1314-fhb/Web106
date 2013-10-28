71ec492f586fb942e4d3acac469ebc1a77af5e9f
Sven Hornberg <sven.hornberg@gmail.com>
2013-10-28 15:44:24 +0100

    grails 2.3.1

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,10 +2,4 @@ modules = {
     application {
         resource url:'js/application.js'
     }
-
-    customBootstrap {
-        dependsOn 'bootstrap'
-        resource url: 'css/scaffolding.css'
-        resource id:'font-awesome-fix', url:[dir: 'less', file: 'font-awesome.less'], attrs:[rel: "stylesheet/less", type:'css', order:100], disposition: 'head'
-    }
 }
