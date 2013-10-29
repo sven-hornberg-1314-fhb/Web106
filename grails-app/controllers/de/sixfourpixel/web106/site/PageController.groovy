ebe44616e622d34d1f97367e96b9e3f847c66926
Sven Hornberg <sven.hornberg@gmail.com>
2013-10-29 19:35:19 +0100

    JSON exsample route

--- grails-app/controllers/de/sixfourpixel/web106/site/PageController.groovy
+++ grails-app/controllers/de/sixfourpixel/web106/site/PageController.groovy
@@ -1,10 +1,20 @@
 package de.sixfourpixel.web106.site
 
-
+import de.sixfourpixel.web106.dyntostatic.CrawlerService
+import grails.converters.JSON
 
 import static org.springframework.http.HttpStatus.*
 
 class PageController {
 
     static scaffold = true
+
+    def sven() {
+        def objTest = new Expando()
+        objTest.name = "Sven"
+        objTest.alter = "27"
+        String textInhalt = objTest.properties as JSON
+        render(text: textInhalt ,contentType:"text/json",encoding:"UTF-8")
+
+    }
 }
