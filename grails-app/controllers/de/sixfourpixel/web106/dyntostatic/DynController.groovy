3336a75b739e189d65f580cca215bd25e8d6e959
Sven Hornberg <sven.hornberg@gmail.com>
2013-11-10 18:25:34 +0100

    new HTMLUtils , first crawl implementation

--- grails-app/controllers/de/sixfourpixel/web106/dyntostatic/DynController.groovy
+++ grails-app/controllers/de/sixfourpixel/web106/dyntostatic/DynController.groovy
@@ -3,6 +3,13 @@ package de.sixfourpixel.web106.dyntostatic
 import grails.converters.JSON
 import grails.plugins.rest.client.RestBuilder
 import groovy.xml.MarkupBuilder
+import groovy.test.GroovyAssert;
+import groovy.util.XmlSlurper
+import groovy.util.slurpersupport.NodeChild;
+
+import de.sixfourpixel.web106.dyntostatic.HtmlUtil
+
+
 
 class DynController {
 
@@ -52,19 +59,45 @@ class DynController {
 
     def crawl() {
 
-        //def contents =  render(controller: defaultAction)
-        //println(contents)
+		String startUrl = "http://localhost:8080/Web106/dyn/";
 
-        //crawlService.crawlPage(defaultAction.toString())
-        render(text:"trigger crawling",contentType:"text/html",encoding:"UTF-8")
-
-        println(request.requestURL)
-
-        def rest = new RestBuilder()
-
-
-        def resp = rest.get("http://grails.org/api/v1.0/plugin/acegi/")
-
-        println(resp.text)
+      
+		Stack<String> toDoLinks = new Stack<String>();
+		def crawledLinks = []
+		toDoLinks.push(startUrl);
+		
+		while (!toDoLinks.isEmpty()) {
+			String nextLink = toDoLinks.pop()
+			crawledLinks.add(nextLink)
+			
+			List<String> links =  crawlLink(nextLink)
+			links.each {
+				if(!it.equals("crawl")) {
+					String newLink =startUrl + it;
+					if(!(newLink in crawledLinks)) {
+						crawledLinks.add(newLink);
+						toDoLinks.push(newLink);
+					}
+				}
+			}
+		}
+	//	if(!startUrl in crawledLinks) {
+			
+			
+	//	}
+		def calendar = Calendar.instance
+			
+		render(text: calendar.time.toString()+ "<br>" + crawledLinks,contentType:"text/html",encoding:"UTF-8")
     }
+	
+	private static List<String> crawlLink(String link) {
+		def rest = new RestBuilder()
+		def htmlUtil = new HtmlUtil();
+		def resp = rest.get(link)
+		List<String> internalLinks = htmlUtil.parseInternalLinks(resp.text)
+		return internalLinks;
+	} 
+	
+
+	
 }
---
+++ grails-app/utils/de/sixfourpixel/web106/dyntostatic/HtmlUtil.groovy
@@ -0,0 +1,31 @@
+package de.sixfourpixel.web106.dyntostatic
+
+import grails.converters.JSON
+import grails.plugins.rest.client.RestBuilder
+import groovy.xml.MarkupBuilder
+import groovy.test.GroovyAssert;
+import groovy.util.XmlSlurper
+import groovy.util.slurpersupport.NodeChild;
+
+class HtmlUtil {
+
+	
+	def parseInternalLinks(String htmlcode) {
+		def rootNode = new XmlSlurper().parseText(htmlcode)
+				
+		List<NodeChild> linkNodes = []
+		List<String> links = []
+		links.add("http://www.google.de")
+		
+		//find all link tags
+		rootNode.'**'.findAll { it.name() == 'a' }.each { linkNodes.add(it) }
+		
+		//get href of each link
+		linkNodes.each {
+			links.add(it.@href.text())
+		}
+		
+		//filter external links
+		return links.findAll { !it.startsWith("http") }			
+	}	
+}
