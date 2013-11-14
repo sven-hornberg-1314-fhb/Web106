df09c24bb2342c79b4f137b4f3275bea0e28a67a
Sven Hornberg <sven.hornberg@gmail.com>
2013-11-14 19:42:07 +0100

    shellscript: unzip override existing files

--- shell_script/ubuntu1204dev.sh
+++ shell_script/ubuntu1204dev.sh
@@ -17,15 +17,15 @@ cd ~/
 mkdir grails
 cd ~/grails
 wget http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.3.1.zip
-unzip grails-2.3.1.zip
+unzip -o grails-2.3.1.zip
 
 #maven
 wget http://www.motorlogy.com/apache/maven/maven-3/3.1.1/binaries/apache-maven-3.1.1-bin.zip
-unzip apache-maven-3.1.1-bin.zip
+unzip -o apache-maven-3.1.1-bin.zip 
 
 #gradle
 wget http://services.gradle.org/distributions/gradle-1.8-all.zip
-unzip gradle-1.8-all.zip
+unzip -o gradle-1.8-all.zip
 
 #java 
 apt-get install openjdk-7-jdk openjdk-7-source openjdk-7-doc openjdk-7-jre-headless openjdk-7-jre-lib -y
