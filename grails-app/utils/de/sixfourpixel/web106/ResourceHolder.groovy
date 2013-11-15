7437e9c10c8aa6defea35fcf34ee48f62a05ef21
marcel-hinderlich-1314-fhb <marcelh89@googlemail.com>
2013-11-15 16:30:30 +0100

    resourceURL holder as utils

---
+++ grails-app/utils/de/sixfourpixel/web106/ResourceHolder.groovy
@@ -0,0 +1,18 @@
+package de.sixfourpixel.web106
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: marcman
+ * Date: 15.11.13
+ * Time: 16:13
+ * To change this template use File | Settings | File Templates.
+ */
+class ResourceHolder {
+
+    def static resource =
+        [twitter : 'https://api.twitter.com/1.1/account/verify_credentials.json',
+         google: 'https://www.googleapis.com/oauth2/v3/userinfo',
+         live: '',
+         yahoo: '' ]
+
+}
