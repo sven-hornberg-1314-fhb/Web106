a974a868c48b03ded7f2a1279e6ef4f6f75ed01f
sven-hornberg-1314-fhb <sven.hornberg@gmail.com>
2013-11-17 14:20:53 +0100

    no newline needed

--- shell_script/ubuntu1204dev.sh
+++ shell_script/ubuntu1204dev.sh
@@ -53,12 +53,12 @@ echo "GRADLE_HOME=/home/$user/grails/gradle-1.8" >> .bashrc
 
 echo "PATH=\$PATH:\$HOME/.local/bin:\$HOME/bin:\$GRAILS_HOME/bin:\$M2_HOME/bin:\$JAVA_HOME/bin:\$GRADLE_HOME/bin" >> .bashrc
 
-echo "export JAVA_HOME\n" >> .bashrc 
-echo "export M2_HOME\n" >> .bashrc 
-echo "export GRAILS_HOME\n" >> .bashrc 
-echo "export MAVEN_REPO\n" >> .bashrc 
-echo "export GRADLE_HOME\n" >> .bashrc 
-echo "export PATH\n" >> .bashrc
+echo "export JAVA_HOME" >> .bashrc 
+echo "export M2_HOME" >> .bashrc 
+echo "export GRAILS_HOME" >> .bashrc 
+echo "export MAVEN_REPO" >> .bashrc 
+echo "export GRADLE_HOME" >> .bashrc 
+echo "export PATH" >> .bashrc
 
 ###last update & upgrade
 apt-get update && apt-get upgrade -y
