7ae5f225566e5a8d85ef847cdf1e12f9ddb7050e
Sven Hornberg <sven.hornberg@gmail.com>
2013-11-20 22:08:40 +0100

    DummyUtils removed

--- grails-app/utils/DummyUtils.groovy
+++
@@ -1,9 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * User: marcman
- * Date: 14.11.13
- * Time: 15:03
- * To change this template use File | Settings | File Templates.
- */
-class DummyUtils {
-}
