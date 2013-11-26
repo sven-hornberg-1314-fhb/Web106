fff36fe530deea20202942c962c45afec923832a
Tanja <tanja@leschke.com>
2013-11-26 12:47:55 +0100

    Rename UUpload File to UploadFile

--- test/unit/web106/file/upload/UUploadFileControllerSpec.groovy
+++ test/unit/web106/file/upload/UUploadFileControllerSpec.groovy
@@ -6,7 +6,7 @@ import spock.lang.Specification
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
-@TestFor(UUploadFileController)
+@TestFor(UploadFileController)
 class UUploadFileControllerSpec extends Specification {
 
     def setup() {
