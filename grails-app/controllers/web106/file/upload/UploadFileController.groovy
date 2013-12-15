9094331c5b9d66a19c648e21e5eeadecd338d858
tanja <tanjaleschke@yahoo.de>
2013-12-15 14:46:17 +0100

    FileUpload Master

---
+++ grails-app/controllers/web106/file/upload/UploadFileController.groovy
@@ -0,0 +1,31 @@
+package web106.file.upload
+
+import upload.convert.FileTypeConverter
+import web106.UserUtils
+import web106.auth.User
+
+class UploadFileController {
+
+    def index() {
+        render(view:"create");
+    }
+	
+	def create() {
+	
+    }
+	
+	def upload() {
+		def mail  = UserUtils.newInstance().emailFromCurrentUser
+		User currentUser = User.find {email== mail}
+
+		def file = request.getFile('file')
+		if(file.empty) {
+			flash.message = "File cannot be empty"
+		} else {
+			FileTypeConverter.convert(file, mail)
+
+		}
+		
+		redirect (action:'create')
+	}
+}
