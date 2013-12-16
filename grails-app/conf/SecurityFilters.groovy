c6d15d14ce835de8b4a8acdbd45a47061cf2b67a
marcel-hinderlich-1314-fhb <marcelh89@googlemail.com>
2013-12-16 18:57:12 +0100

    Prints entfernen, currentwebsite.page entfernen, appconf update

--- grails-app/conf/SecurityFilters.groovy
+++ grails-app/conf/SecurityFilters.groovy
@@ -2,24 +2,21 @@ import web106.site.Page
 import web106.site.Website
 
 /**
-* Our filter to sanitize URLs. Grails detects any Groovy class inside the
+* filter to secure URLs ( guessing sequencial IDs ).
+* Grails detects any Groovy class inside the
 * grails-app/conf folder ending with 'Filters' to be a filter by convention.
 *
 * before - Applied before processing a request URL.
 * after - Applied after processing a request URL, but before rendering the view.
 * afterView - Applied after rendering the view.
 *
-* @author lasantha
 */
 class SecurityFilters {
-    // All filters are defined is this 'filters' block
+
     def filters = {
-        // 'urlSanitizeFilter' is the name of our filter.Specify any name.
-        // controller:'student',action:'*' means 
-        // 'All actions in student controller'
-        // If you want to capture all actions, use controller:'*',action:'*'
+
         urlSanitizeFilter(controller:'page',action:'*') {
-            // can be one of 'before','after','afterView'
+
             before = {
                 if(params?.id) { // if a parameter named 'id' is passed in.
                     // if id is not a number of 1-10 digits,
@@ -41,8 +38,6 @@ class SecurityFilters {
                             redirect(controller: 'errors', action: 'accessDenied', params:[message:'access denied'])
                         }
 
-                        print "page exists "+currentpage
-
                         //get current website
                         def currentWebsite = Website.find{
                                      id == activeWebsiteId
@@ -52,16 +47,12 @@ class SecurityFilters {
                             redirect(controller: 'errors', action: 'accessDenied', params:[message:'access denied'])
                         }
 
-                        //get pages of current website
-                        currentWebsite.page
-                        //check if website contain current page
+                        //check if website contains current page
                         if(!currentpage in currentWebsite.page){
                             redirect(controller: 'errors', action: 'accessDenied', params:[message:'access denied'])
                         }else{
-                            print "currentpage is in current websites pages list"
-
                             //compare workgroup of current and session page, redirect if not equal
-                            if(!currentWebsite.workGroupId ==  activeWebsiteId){
+                            if(!currentWebsite.workGroupId ==  activeWorkGroupId){
                                 redirect(controller: 'errors', action: 'accessDenied', params:[message:'access denied'])
                             }
 
@@ -72,9 +63,7 @@ class SecurityFilters {
         }
 
 
-        // You can add other filters here. The order in which filters 
-        // are matched will be determined by the order in which they appear 
-       // in the 'filters' block. Filters are matched from top to bottom.
+        // add other filters here. Filters are executed from top to bottom.
    }
 }
 
