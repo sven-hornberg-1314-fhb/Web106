9788e2a0640459f26b9d398666ded039066539ef
tanja <tanjaleschke@yahoo.de>
2014-01-02 10:42:24 +0100

    ImageController fuer Gallerie angepasst

--- grails-app/controllers/web106/imagegallery/ImageController.groovy
+++ grails-app/controllers/web106/imagegallery/ImageController.groovy
@@ -4,13 +4,25 @@ import access.s3.ImageService
 import upload.convert.FileTypeConverter
 import web106.UserUtils
 import web106.auth.User
+import web106.site.Website
 
 class ImageController {
 
     def index() { 
+		def activeWebsite
+		
+		def activeWebsiteSession = session.getAttribute('activeWebsite')
+		activeWebsite = activeWebsiteSession
+		Website website = Website.find {
+			id == activeWebsite
+		}
+		String websitename = website.title + "-Images"
+		print websitename
+
 		def mail  = UserUtils.newInstance().emailFromCurrentUser
 		User currentUser = User.find {email== mail}
-		String newmail = FileTypeConverter.replaceAT(mail)
+		
+		String newmail = FileTypeConverter.replaceAT(mail+ "." + websitename)
 		print newmail
 		
 		ImageService progress = new ImageService(newmail);
