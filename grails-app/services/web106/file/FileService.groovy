67df17c971bf05310d968fe0493ecf28b256ebee
Sven Hornberg <sven.hornberg@gmail.com>
2014-01-04 17:09:37 +0100

    Fileservice groovy

---
+++ grails-app/services/web106/file/FileService.groovy
@@ -0,0 +1,60 @@
+package web106.file
+
+import grails.transaction.Transactional
+
+@Transactional
+class FileService {
+
+
+    File createTempFile(String folder, String fileName, String content) {
+
+        String tDir = System.getProperty("java.io.tmpdir");
+        File tempFile = null;
+        if(folder != null || folder != "") {
+
+            tempFile = new File(tDir + "/" + folder + "/" + fileName);
+
+        } else {
+
+            tempFile = new File(tDir + "/" + fileName);
+        }
+        try {
+            BufferedWriter output = new BufferedWriter(new FileWriter(tempFile));
+            output.write(content);
+            output.close();
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+
+    }
+
+    File deleteTempFile(String folder, String fileName) {
+
+        def returnVal = false
+
+        String tDir = System.getProperty("java.io.tmpdir");
+        File tempFile = null;
+        try{
+
+            if(folder != null || folder != "") {
+
+                tempFile = new File(tDir + "/" + folder + "/" + fileName);
+
+            } else {
+
+                tempFile = new File(tDir + "/" + fileName);
+            }
+
+
+            if(tempFile.delete()){
+                returnVal = true
+            }
+
+        }catch(Exception e){
+
+            e.printStackTrace();
+        }
+
+        return returnVal
+    }
+}
---
+++ test/unit/web106/FileServiceSpec.groovy
@@ -0,0 +1,21 @@
+package web106
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+import web106.file.FileService
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(FileService)
+class FileServiceSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
