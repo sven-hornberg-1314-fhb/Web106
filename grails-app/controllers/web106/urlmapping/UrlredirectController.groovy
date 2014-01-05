acf115abc6443636d1418a7cba06692e371d554d
marcel-hinderlich-1314-fhb <marcelh89@googlemail.com>
2014-01-05 20:42:42 +0100

    redirect for live view on s3

--- grails-app/controllers/web106/urlmapping/UrlredirectController.groovy
+++ grails-app/controllers/web106/urlmapping/UrlredirectController.groovy
@@ -41,7 +41,34 @@ class UrlredirectController {
 		
 	}
 
+    /**
+     * e.g. http://localhost:8080/Web106/live/testexportbucket-webseite/index.html
+     *      Path: /live/testexportbucket-website/index.html
+     */
     def live() {
+
+        def workgroupAndWebsite
+        def workgroupName
+        def websiteName
+        def pageName = params.website
+
+        workgroupAndWebsite = params.workgroup
+
+        if(workgroupAndWebsite==null || workgroupAndWebsite==''){
+            //error no website for live view
+        }
+
+        if(pageName == null || pageName == ''){
+            //error no page for live view
+        }
+
+        workgroupName = workgroupAndWebsite.substring(0, workgroupAndWebsite.indexOf('-'))
+
+        websiteName =  workgroupAndWebsite.substring(workgroupAndWebsite.indexOf('-')+1)
+
+        def url = 'https://s3-eu-west-1.amazonaws.com/'+workgroupName+'-'+websiteName+'/'+pageName
+
+        redirect(url: url)
 
     }
 	
