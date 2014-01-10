8818bbc81f75a0a2ccc9b5aec5a30f281b7c928c
Sven Hornberg <sven.hornberg@gmail.com>
2014-01-10 21:52:49 +0100

    reduced timer for initjob

--- grails-app/jobs/web106/InitJob.groovy
+++ grails-app/jobs/web106/InitJob.groovy
@@ -8,9 +8,7 @@ class InitJob {
     def BootStrapWeb106Service bootStrapWeb106Service
 
     static triggers = {
-        simple name:'initTrigger', startDelay:10000
-        //cron name:'cronTrigger', startDelay:10000, cronExpression: '0 0/30 * * * ?' //wiederhole alle minute
-        //custom name:'customTrigger', triggerClass:MyTriggerClass, myParam:myValue, myAnotherParam:myAnotherValue
+        simple name:'initTrigger', startDelay:5000
     }
 
     def execute() {
