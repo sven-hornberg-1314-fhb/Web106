150a895ff264fcf643c0d1b73be0f6e0e6f42d2d
Sven Hornberg <sven.hornberg@gmail.com>
2014-01-16 14:48:48 +0100

    Test: TemplateController

--- test/integration/web106/TemplateControllerSpec.groovy
+++ test/integration/web106/TemplateControllerSpec.groovy
@@ -1,23 +1,31 @@
 package web106
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.*
+import web106.auth.Role
 import web106.template.TemplateController
 
 /**
  * spocktests for templates
  */
 @TestFor(TemplateController)
+@Mock([Role])
 class TemplateControllerSpec extends Specification {
 
     void "Defaultmodel Berlin contains Boxes"() {
 
+        setup:
+            def templateController = new TemplateController()
+
         when:
-            controller.berlin()
+            templateController.berlin()
+            def test = templateController.response.contentAsString
 
         then:
-            response.status == 200
-
+           templateController.response.contentAsString != null
+           templateController.response.contentAsString.contains('sidebar1')
+           templateController.response.contentAsString.contains('footer2')
 
     }
 
