9902a9759f2da0214c9179a12b36c0b1f2879a49
Sven Hornberg <sven.hornberg@gmail.com>
2014-01-16 15:04:42 +0100

    Test: TemplateController

--- test/integration/web106/TemplateControllerSpec.groovy
+++ test/integration/web106/TemplateControllerSpec.groovy
@@ -13,19 +13,32 @@ import web106.template.TemplateController
 @Mock([Role])
 class TemplateControllerSpec extends Specification {
 
-    void "Defaultmodel Berlin contains Boxes"() {
+    def "Berlin contains Boxes"() {
 
         setup:
             def templateController = new TemplateController()
 
         when:
             templateController.berlin()
-            def test = templateController.response.contentAsString
 
         then:
            templateController.response.contentAsString != null
            templateController.response.contentAsString.contains('sidebar1')
            templateController.response.contentAsString.contains('footer2')
+
+    }
+
+    def "Check names"() {
+
+        setup:
+            def templateController = new TemplateController()
+
+        when:
+            def nameList = templateController.listNames()
+
+        then:
+            nameList.contains('Berlin')
+            nameList.contains('Kairo')
 
     }
 
