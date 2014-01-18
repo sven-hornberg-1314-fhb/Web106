6fe64e88a2c62cb7fc55999b53deeabb601ebd28
marcel-hinderlich-1314-fhb <marcelh89@googlemail.com>
2014-01-18 18:56:26 +0100

    context root /

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -11,6 +11,8 @@
 //    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
 // }
 
+grails.app.context = '/'
+
 grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
 
 // The ACCEPT header will not be used for content negotiation for user agents containing the following strings (defaults to the 4 major rendering engines)
@@ -98,12 +100,12 @@ grails.hibernate.cache.queries = false
 environments {
     development {
         grails.logging.jul.usebridge = true
-	grails.serverURL = "http://localhost:8080/${appName}"
+	grails.serverURL = "http://localhost:8080/"
     }
 
     test {
         grails.logging.jul.usebridge = true
-        grails.serverURL = "http://localhost:8080/${appName}"
+        grails.serverURL = "http://localhost:8080/"
     }
     production {
         grails.logging.jul.usebridge = false
